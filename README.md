63ebedd740d37bad4a8c090a92dd73443542d77a
lucile gz <xhcs1998@gmail.com>
2024-03-12 12:54:46 -0600

    upload all files (2)

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # ENGO551-Lab4
+This web map application is an interactive web map application to visualize the vehicle incidents in Calgary in 2017. It shows users the location of each incident and its information including description, incident info, and the time when it happended.
+
+The style of the map application comes from Mapbox and most of the applications are done using mapbox gl js API.
+
+On the web page there are two buttons: layer button and refresh botton. When the user clicks on the layer button, the incidents layers are hiddle or shown based on the previous state. When the user clicks on the refresh button, the web page will get back to the default setup with all the layers shown.
+
+There are two layers in the map to visualize the incidents: cluster layer and uncluster point layer. Cluster layer contains multiple incidents in big circles. The circle tells you the number of incidents it cover. The more the incidents it covers, the larger the circle is and the circle is yellow or red. The cluster circles are set to be 50% transparent by default so that users can find the place they want to look into. However, if the mouse is hovered over the circle, the circle will get non-transparent. Users can click on it to zoom in to see more detail.
+
+The uncluster points appears when they are sparse. Every point represent a single incident happened. Points are red solid dot so that they can be outstanding from the context. The mouse will convert to pointer shape when hovered over the dot. Users can get the incident information by clicking on the dot.
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 
+
 // The value for 'accessToken' begins with 'pk...'
 mapboxgl.accessToken = 'pk.eyJ1Ijoicm95eGlhbyIsImEiOiJjbHRoeXNmNnYwYWFzMmlvMXJ5bXRtaHZuIn0.7wuCPxXiZeN9KyF8oAKPFg'; 
 const map = new mapboxgl.Map({
@@ -10,137 +11,347 @@ const map = new mapboxgl.Map({
 });
 
 
-/*
-Create a marker cluster to group markers
-*/
-map.on('load', ()=> {
-  map.addSource('incidents', {
-    type: 'geojson',
-    data: 'https://api.mapbox.com/datasets/v1/royxiao/cltma5nnl67jc1us4wdzkdqjf/features' + 
-          '?access_token=pk.eyJ1Ijoicm95eGlhbyIsImEiOiJjbHRoeXNmNnYwYWFzMmlvMXJ5bXRtaHZuIn0.7wuCPxXiZeN9KyF8oAKPFg',
-    cluster:  true,
-    clusterMaxZoom: 14,
-    clusterRadius: 50
-  })
+
+fetch('https://raw.githubusercontent.com/chshxiao/ENGO551-Lab4/master/features.geojson')
+.then(response => response.json())
+.then(data => {
+  map.on('load', ()=> {
+
+
+  /*
+  Add data and layers of the incidents in two layers: cluster and unclustered-points
+  The data comes from fetch API heading Github repository
+  */
+    map.addSource('incidents', {
+      type: 'geojson',
+      // data: 'https://api.mapbox.com/datasets/v1/royxiao/cltma5nnl67jc1us4wdzkdqjf/features' + 
+      //       '?access_token=pk.eyJ1Ijoicm95eGlhbyIsImEiOiJjbHRoeXNmNnYwYWFzMmlvMXJ5bXRtaHZuIn0.7wuCPxXiZeN9KyF8oAKPFg',
+      data: data,
+      cluster:  true,
+      clusterMaxZoom: 14,
+      clusterRadius: 50
+    })
+    
+    map.addLayer({
+      id: 'clusters',
+      type: 'circle',
+      source: 'incidents',
+      filter: ['has', 'point_count'],
+      paint: {
+        'circle-color': [
+          'step',
+          ['get', 'point_count'],
+          '#51bbd6',
+          100,
+          '#f1f075',
+          750,
+          '#f28cb1'
+        ],
+        'circle-radius': [
+          'step',
+          ['get', 'point_count'],
+          20,
+          100,
+          30,
+          750,
+          40
+        ],
+        'circle-opacity': [
+          'case',
+          ['boolean', ['feature-state', 'hover'], false],
+          1,
+          0.5
+        ]
+      },
+      layout: {
+        'visibility': 'visible'
+      }
+    });
   
-  const test = fetch('https://api.mapbox.com/datasets/v1/royxiao/cltma5nnl67jc1us4wdzkdqjf/features' + 
-                      '?access_token=pk.eyJ1Ijoicm95eGlhbyIsImEiOiJjbHRoeXNmNnYwYWFzMmlvMXJ5bXRtaHZuIn0.7wuCPxXiZeN9KyF8oAKPFg')
-              .then(response => response.json())
-              .then(data => {
-                console.log(data);
-              })
-
-  map.addLayer({
-    id: 'clusters',
-    type: 'circle',
-    source: 'incidents',
-    filter: ['has', 'point_count'],
-    paint: {
-      'circle-color': [
-        'step',
-        ['get', 'point_count'],
-        '#51bbd6',
-        100,
-        '#f1f075',
-        750,
-        '#f28cb1'
-      ],
-      'circle-radius': [
-        'step',
-        ['get', 'point_count'],
-        20,
-        100,
-        30,
-        750,
-        40
-      ]
-    }
-  });
-
-  map.addLayer({
-    id: 'cluster-count',
-    type: 'symbol',
-    source: 'incidents',
-    filter: ['has', 'point_count'],
-    layout: {
-      'text-field': ['get', 'point_count_abbreviated'],
-      'text-font': ['DIN Offc Pro Medium', 'Arial Unicode MS Bold'],
-      'text-size': 12
-    }
-  });
-
-  map.addLayer({
-    id: 'unclustered-point',
-    type: 'circle',
-    source: 'incidents',
-    filter: ['!', ['has', 'point_count']],
-    paint: {
-      'circle-color': '#d51b21',
-      'circle-radius': 3,
-      'circle-stroke-width': 1,
-      'circle-stroke-color': '#fff'
-    }
-  });
-
-  // inspect a cluster on a click
-  map.on('click', 'clusters', (e) => {
-    const features = map.queryRenderedFeatures(e.point, {
-      layers: ['clusters']
+    map.addLayer({
+      id: 'cluster-count',
+      type: 'symbol',
+      source: 'incidents',
+      filter: ['has', 'point_count'],
+      layout: {
+        'text-field': ['get', 'point_count_abbreviated'],
+        'text-font': ['DIN Offc Pro Medium', 'Arial Unicode MS Bold'],
+        'text-size': 12,
+        'visibility': 'visible'
+      }
+    });
+  
+    map.addLayer({
+      id: 'unclustered-point',
+      type: 'circle',
+      source: 'incidents',
+      filter: ['!', ['has', 'point_count']],
+      paint: {
+        'circle-color': '#d51b21',
+        'circle-radius': 5,
+        'circle-stroke-width': 1,
+        'circle-stroke-color': '#fff'
+      },
+      layout: {
+        'visibility': 'visible'
+      }
     });
 
-    const cluster_id = features[0].properties.cluster_id;
-    map.getSource('incidents').getClusterExpansionZoom(
-      cluster_id,
-      (err, zoom) => {
-        if (err) return;
 
-        map.easeTo({
-          center: features[0].geometry.coordinates,
-          zoom: zoom+1
-        });
-      }
-    )
-  });
+    /**
+    The function to toggle layers on
+    Used in refresh button and function toggleOnAndOff
+    */
+    function toggleOn() {
+      map.setLayoutProperty(
+        'clusters',
+        'visibility',
+        'visible'
+      );
 
-  // When a click event occurs on a feature in
-  // the unclustered-point layer, open a popup at
-  // the location of the feature, with
-  // description HTML from its properties.
-  map.on('click', 'unclustered-point', (e) => {
-    const coordinates = e.features[0].geometry.coordinates.slice();
-    const info = e.features[0].properties['INCIDENT INFO'];
-    const description = e.features[0].properties.DESCRIPTION;
-    const date = e.features[0].properties.START_DT;
+      map.setLayoutProperty(
+        'unclustered-point',
+        'visibility',
+        'visible'
+      );
 
-    // Ensure that if the map is zoomed out such that
-    // multiple copies of the feature are visible, the
-    // popup appears over the copy being pointed to.
-    while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
-      coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+      map.setLayoutProperty(
+        'cluster-count',
+        'visibility',
+        'visible'
+      );
     }
 
-    new mapboxgl.Popup()
-    .setLngLat(coordinates)
-    .setHTML(
-      `${info}<br>${description}<br>${date}`
-    )
-    .addTo(map);
+
+    /*
+    The function to toggle layers on and off
+    Used in layer button and refresh button
+    */
+    function toggleOnAndOff() {
+      // get the visibility properties of layers
+      const cluster_visibility = map.getLayoutProperty(
+        'clusters',
+        'visibility'
+      );
+
+      const uncluster_visibility = map.getLayoutProperty(
+        'unclustered-point',
+        'visibility'
+      );
+
+      const cluster_num_visibility = map.getLayoutProperty(
+        'cluster-count',
+        'visibility'
+      )
+
+      // toggle off the layers if any of the layer is visible
+      if (cluster_visibility === 'visible' || uncluster_visibility === 'visible' || cluster_num_visibility === 'visible')
+      {
+        map.setLayoutProperty(
+          'clusters',
+          'visibility',
+          'none'
+        );
+
+        map.setLayoutProperty(
+          'unclustered-point',
+          'visibility',
+          'none'
+        );
+
+        map.setLayoutProperty(
+          'cluster-count',
+          'visibility',
+          'none'
+        );
+      } else {
+        map.setLayoutProperty(
+          'clusters',
+          'visibility',
+          'visible'
+        );
+
+        map.setLayoutProperty(
+          'unclustered-point',
+          'visibility',
+          'visible'
+        );
+
+        map.setLayoutProperty(
+          'cluster-count',
+          'visibility',
+          'visible'
+        );
+      }
+    };
+
+
+    /*
+    the interaction with the button to toggle layers on and off
+    */
+    map.on('idle', () => {
+      
+      // If these two layers were not added to the map, abort
+      if (!map.getLayer('clusters') || !map.getLayer('unclustered-point')) {
+        return;
+      }
+
+      document.querySelector('#layerButton').onclick = function () {
+        toggleOnAndOff();
+      }
+
+      // // Enumerate ids of the layers.
+      // const toggleableLayerIds = ['contours', 'museums'];
+
+      // // Set up the corresponding toggle button for each layer.
+      // for (const id of toggleableLayerIds) {
+
+      //   // Skip layers that already have a button set up.
+      //   if (document.getElementById(id)) {
+      //     continue;
+      //   }
+
+      //   // Create a link.
+      //   const link = document.createElement('a');
+      //   link.id = id;
+      //   link.href = '#';
+      //   link.textContent = id;
+      //   link.className = 'active';
+
+      //   // Show or hide layer when the toggle is clicked.
+      //   link.onclick = function (e) {
+      //     const clickedLayer = this.textContent;
+      //     e.preventDefault();
+      //     e.stopPropagation();
+
+      //     const visibility = map.getLayoutProperty(
+      //       clickedLayer,
+      //       'visibility'
+      //     );
+
+      //     // Toggle layer visibility by changing the layout object's visibility property.
+      //     if (visibility === 'visible') {
+      //       map.setLayoutProperty(clickedLayer, 'visibility', 'none');
+      //       this.className = '';
+      //     } else {
+      //       this.className = 'active';
+      //       map.setLayoutProperty(
+      //         clickedLayer,
+      //         'visibility',
+      //         'visible'
+      //       );
+      //     }
+      //   };
+      // }
+    });
+
+
+    //refresh the page when the user clicks the refresh button
+    map.on('idle', () => {
+      document.querySelector('#refreshButton').onclick = function () {
+        // set the default view
+        map.setCenter([-114.05, 51.02]);
+        map.setZoom(10.7);
+        
+        // set the layers back to visible
+        toggleOn();
+      }
+    })
+
+
+    // inspect a cluster on a click
+    map.on('click', 'clusters', (e) => {
+      const features = map.queryRenderedFeatures(e.point, {
+        layers: ['clusters']
+      });
+  
+      const cluster_id = features[0].properties.cluster_id;
+      map.getSource('incidents').getClusterExpansionZoom(
+        cluster_id,
+        (err, zoom) => {
+          if (err) return;
+  
+          map.easeTo({
+            center: features[0].geometry.coordinates,
+            zoom: zoom+1
+          });
+        }
+      )
+    });
+  
+    // When a click event occurs on a feature in
+    // the unclustered-point layer, open a popup at
+    // the location of the feature, with
+    // description HTML from its properties.
+    map.on('click', 'unclustered-point', (e) => {
+      const coordinates = e.features[0].geometry.coordinates.slice();
+      const info = e.features[0].properties['INCIDENT INFO'];
+      const description = e.features[0].properties.DESCRIPTION;
+      const date = e.features[0].properties.START_DT;
+  
+      // Ensure that if the map is zoomed out such that
+      // multiple copies of the feature are visible, the
+      // popup appears over the copy being pointed to.
+      while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+        coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+      }
+  
+      new mapboxgl.Popup()
+      .setLngLat(coordinates)
+      .setHTML(
+        `${info}<br>${description}<br>${date}`
+      )
+      .addTo(map);
+    });
+  
+    // points to the features with mouse hover
+    let feature_id;
+
+    map.on('mouseenter', 'clusters', (e) => {
+      map.getCanvas().style.cursor = 'pointer';
+
+      // update the features to a hover state
+      if (e.features.length > 0) {
+        feature_id = e.features[0].id;                  // point the feature_id to the hovered feature
+        map.setFeatureState({
+          source: 'incidents',
+          id: feature_id,
+        },
+        {
+          hover: true,
+        });
+      } 
+    });
+    map.on('mouseleave', 'clusters', () => {
+      map.getCanvas().style.cursor = '';
+
+      // update the features to a hover state
+      map.setFeatureState({
+        source: 'incidents',
+        id: feature_id,
+      },
+      {
+        hover: false,
+      });
+      feature_id = null;                             // null the feature id
+    });
+  
+    map.on('mouseenter', 'unclustered-point', () => {
+      map.getCanvas().style.cursor = 'pointer';
+    });
+    map.on('mouseleave', 'unclustered-point', () => {
+      map.getCanvas().style.cursor = '';
+    });
+
+
   });
 
-  map.on('mouseenter', 'clusters', () => {
-    map.getCanvas().style.cursor = 'pointer';
-  });
-  map.on('mouseleave', 'clusters', () => {
-    map.getCanvas().style.cursor = '';
-  });
 
-  map.on('mouseenter', 'unclustered-point', () => {
-    map.getCanvas().style.cursor = 'pointer';
-  });
-  map.on('mouseleave', 'unclustered-point', () => {
-    map.getCanvas().style.cursor = '';
-  });
-});
+
+
+
+})
+
 
 
 // /* 
